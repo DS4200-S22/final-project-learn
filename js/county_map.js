d0425b20168a399610a18108d70891494b6f29b2
kreenatotala <74081275+kreenatotala@users.noreply.github.com>
2022-04-07 03:51:56 -0400

    County Map with Tooltips
    Functional county map that currently displays the name of each county upon hovering.

--- js/county_map.js
+++ js/county_map.js
@@ -1,121 +1,75 @@
-let width = 615;
-let height = 375;
-let yTooltipOffset = 15; 
 
-let projection = d3.geoMercator()
-                    .center([0, 0])
-                    .scale(width / 2.5 / Math.PI)
-                    .rotate([0, 0])
-                    .translate([width / 2, height / 2]);
 
-let path = d3.geoPath().projection(null);
+// let usData = await d3.json('https://d3js.org/us-10m.v2.json');
 
-let svg = d3
+// let stateData = topojson.feature(usData, usData.objects.states).features.filter((d) => d.id === selectedStateId);
+
+// let countiesData = topojson.feature(usData, usData.objects.counties).features;
+
+// let projection1 = d3.geoIdentity().fitSize([width, height], stateData[0]);
+
+
+let svg1 = d3
   .select("#vis-container2")
   .append("svg")
   .attr("width", 615)
   .attr("height", 375);
 
-let g = svg.append("g").attr("class", "g-town");
+let g1 = svg1.append("g").attr("class", "g-county");
 
-const tooltip1 = d3.select("#vis-container2")
-                    .append("div")
-                    .attr('id', "tooltip1")
-                    .style("opacity", 0)
-                    .attr("class", "tooltip");
-
-const mouseover1 = function(event, d) {
-  tooltip1.html("School Name: [INSERT SCHOOL NAME HERE], School Type: Public, County: [County Name]");
-}
-
-const mousemove1 = function(event, d) {
-  tooltip1.style("left", (event.pageX)+"px") 
-          .style("top", (event.pageY + yTooltipOffset) +"px"); 
-}
-
-// This creates a constant that represents a function that
-// sets the tooltip opacity back to 0 (invisible) after the mouse
-// leaves the object
-const mouseleave1 = function(event, d) { 
-  tooltip1.style("opacity", 0); 
-}
-
-// Handmade legend
-svg.append("circle").attr("cx",10).attr("cy",330).attr("r", 6).style("fill", "deepskyblue")
-svg.append("circle").attr("cx",10).attr("cy",360).attr("r", 6).style("fill", "salmon")
-svg.append("text").attr("x", 20).attr("y", 331.5).text("Teaches CS").style("font-size", "15px").attr("alignment-baseline","middle")
-svg.append("text").attr("x", 20).attr("y", 361.5).text("Does Not Teach CS").style("font-size", "15px").attr("alignment-baseline","middle")
 
 // load and display the Massachusetts map
-d3.json("../data/ma-counties.topojson").then(function (topology) {
-  g.selectAll("path")
-    .data(topojson.feature(topology, topology.objects.counties).features)
+d3.json("../data/ma-counties2.topojson").then(function (topology) {
+    
+    //if (error) throw error;
+
+    let datum = topojson.feature(topology, topology.objects.cb_2015_massachusetts_county_20m).features;
+    console.log(datum);
+
+    let projection1 = d3.geoMercator()
+                    .center([0, 0])
+                    //.scale(5000);
+                    .scale(7000)
+                    .rotate([0, 0])
+                    .translate([9000, 5800]);
+
+    //let featureCollection = {"type":"featureCollection","features":topology};
+    //projection1.fitSize([615, 375], featureCollection);
+
+    //console.log(featureCollection);
+
+    let path1 = d3.geoPath().projection(projection1);
+
+    // var tooltipDiv = d3.select("body").append("div")    
+    // .attr("class", "tooltip")               
+    // .style("opacity", 0);
+
+  g1.selectAll("path")
+    .data(datum)
     .enter()
     .append("path")
     .attr("class", "county")
-    .attr("d", path)
-    .style("stroke", "white");
+    .attr("d", path1)
+    .style("stroke", "white")
+    .on('mouseover', function(d) {
+        mouseEvent(d, this, true);
+    })
+    .on('mouseout', function(d) {
+        mouseEvent(d, this, false);
+        // div.transition()        
+        //     .duration(500)      
+        //     .style("opacity", 0); 
+    })
+    .append('title')
+    .text(d => d.properties.NAME);
 
-//   let data = [
-//     [100, 10, "deepskyblue"],
-//     [10, 280, "deepskyblue"],
-//     [10, 23, "salmon"],
-//     [1, 1, "deepskyblue"],
-//     [-8, -6, "salmon"],
-//     [1, 50, "deepskyblue"],
-//     [-100, -1, "salmon"],
-//     [-104, -1, "deepskyblue"],
-//     [-104, 10, "deepskyblue"],
-//     [-105, 30, "salmon"],
-//     [-130, 50, "deepskyblue"],
-//     [-105, 31, "salmon"],
-//     [-130, 52, "deepskyblue"],
-//     [-150, 30, "salmon"],
-//     [-150, 60, "deepskyblue"],
-//     [-100, 50, "deepskyblue"],
-//     [-9, 31, "salmon"],
-//     [-90, 70, "deepskyblue"],
-//     [-80, 60, "salmon"],
-//     [-75, 50, "deepskyblue"],
-//     [90, -50, "salmon"],
-//     [100, -40, "deepskyblue"],
-//     [100, -60, "salmon"],
-//     [90, -20, "salmon"],
-//     [90, -40, "deepskyblue"],
-//     [80, -60, "deepskyblue"],
-//     [70, -20, "deepskyblue"],
-//     [70, 40, "salmon"],
-//     [80, 42, "deepskyblue"],
-//     [70, 50, "salmon"],
-//     [80, 45, "deepskyblue"],
-//     [75, 40, "deepskyblue"],
-//     [85, 40, "salmon"],
-//     [30, 70, "salmon"],
-//     [80, 75, "deepskyblue"],
-//     [75, 75, "deepskyblue"],
-//     [85, 70, "salmon"],
-//     [-40, 40, "deepskyblue"],
-//     [-180, 40, "salmon"],
-//     [-180, 20, "deepskyblue"],
-//   ];
+    function mouseEvent(d, item, bool) {
+        d3.select(item).classed("selected", bool);
+        //document.getElementById("county-name").innerHTML = d.vessel_name;
+        //d3.select(item).append("div").attr("class", "div.tooltip");
+    };
 
-//   // add circles to svg
-//   svg.selectAll("circle")
-//     .data(data)
-//     .enter()
-//     .append("circle")
-//     .attr("cx", function (d) {
-//       console.log(projection(d));
-//       return projection(d)[0];
-//     })
-//     .attr("cy", function (d) {
-//       return projection(d)[1];
-//     })
-//     .attr("r", "4px")
-//     .attr("fill", function (d) {
-//       return d[2];
-//     })
-//     .on("mouseover", mouseover1)
-//     .on("mousemove", mousemove1)
-//     .on("mouseleave", mouseleave1);
+    // console.log(g);
+    console.log(g1);
+
 });
