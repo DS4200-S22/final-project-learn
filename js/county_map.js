5f0dd715b88bba351cca965d6c3db21237ad0f88
Alina Chiu <chiu.a@northeastern.edu>
2022-04-07 04:43:00 -0400

    formatting

--- js/county_map.js
+++ js/county_map.js
@@ -1,13 +1,4 @@
-
-
 // let usData = await d3.json('https://d3js.org/us-10m.v2.json');
-
-// let stateData = topojson.feature(usData, usData.objects.states).features.filter((d) => d.id === selectedStateId);
-
-// let countiesData = topojson.feature(usData, usData.objects.counties).features;
-
-// let projection1 = d3.geoIdentity().fitSize([width, height], stateData[0]);
-
 
 let svg1 = d3
   .select("#vis-container2")
@@ -17,32 +8,24 @@ let svg1 = d3
 
 let g1 = svg1.append("g").attr("class", "g-county");
 
-
 // load and display the Massachusetts map
-d3.json("../data/ma-counties2.topojson").then(function (topology) {
-    
-    //if (error) throw error;
+d3.json("../data/ma-counties.topojson").then(function (topology) {
+  //if (error) throw error;
 
-    let datum = topojson.feature(topology, topology.objects.cb_2015_massachusetts_county_20m).features;
-    console.log(datum);
+  let datum = topojson.feature(
+    topology,
+    topology.objects.cb_2015_massachusetts_county_20m
+  ).features;
 
-    let projection1 = d3.geoMercator()
-                    .center([0, 0])
-                    //.scale(5000);
-                    .scale(7000)
-                    .rotate([0, 0])
-                    .translate([9000, 5800]);
+  let projection1 = d3
+    .geoMercator()
+    .center([0, 0])
+    //.scale(5000);
+    .scale(7000)
+    .rotate([0, 0])
+    .translate([9000, 5800]);
 
-    //let featureCollection = {"type":"featureCollection","features":topology};
-    //projection1.fitSize([615, 375], featureCollection);
-
-    //console.log(featureCollection);
-
-    let path1 = d3.geoPath().projection(projection1);
-
-    // var tooltipDiv = d3.select("body").append("div")    
-    // .attr("class", "tooltip")               
-    // .style("opacity", 0);
+  let path1 = d3.geoPath().projection(projection1);
 
   g1.selectAll("path")
     .data(datum)
@@ -51,25 +34,21 @@ d3.json("../data/ma-counties2.topojson").then(function (topology) {
     .attr("class", "county")
     .attr("d", path1)
     .style("stroke", "white")
-    .on('mouseover', function(d) {
-        mouseEvent(d, this, true);
+    .on("mouseover", function (d) {
+      mouseEvent(d, this, true);
     })
-    .on('mouseout', function(d) {
-        mouseEvent(d, this, false);
-        // div.transition()        
-        //     .duration(500)      
-        //     .style("opacity", 0); 
+    .on("mouseout", function (d) {
+      mouseEvent(d, this, false);
+      // div.transition()
+      //     .duration(500)
+      //     .style("opacity", 0);
     })
-    .append('title')
-    .text(d => d.properties.NAME);
+    .append("title")
+    .text((d) => d.properties.NAME);
 
-    function mouseEvent(d, item, bool) {
-        d3.select(item).classed("selected", bool);
-        //document.getElementById("county-name").innerHTML = d.vessel_name;
-        //d3.select(item).append("div").attr("class", "div.tooltip");
-    };
-
-    // console.log(g);
-    console.log(g1);
-
+  function mouseEvent(d, item, bool) {
+    d3.select(item).classed("selected", bool);
+    //document.getElementById("county-name").innerHTML = d.vessel_name;
+    //d3.select(item).append("div").attr("class", "div.tooltip");
+  }
 });
--- js/table.js
+++ js/table.js
@@ -1,111 +1,123 @@
+// credit for inspiration for sortable, filterable table: http://bl.ocks.org/dhoboy/1ac430a7ca883e7a8c09
 d3.csv("../data/cs_report.csv").then((data) => {
-	// the columns you'd like to display
-	 columns = [
-	  'School Name',
-	  'Offers CS?',
-	  'num_students',
-	  'School District Name',
-	  'County Name'
-	];
-  
-	 table = d3.select("#table-container").append("table"),
-		thead = table.append("thead"),
-		tbody = table.append("tbody");
-  
-	// append the header row
-	thead.append("tr")
-		.selectAll("th")
-		.data(columns)
-		.enter()
-		.append("th")
-			.text(function(column) { return column; });
-  
-	// create a row for each object in the data
-	 rows = tbody.selectAll("tr")
-		.data(data)
-		.enter()
-		.append("tr");
-  
-	// create a cell in each row for each column
-	 cells = rows.selectAll("td")
-		.data(function(row) {
-			return columns.map(function(column) {
-				return {column: column, value: row[column]};
-			});
-		})
-		.enter()
-		.append("td")
-			.text(function(d) { return d.value; });
-  
-			d3.select("#gene_search_box").on("keyup", function() { // filter according to key pressed 
-			   searched_data = data,
-				text = this.value.trim();
-			
-			 searchResults = searched_data.map(function(r) {
-		  
-			   regex = new RegExp(text);
-			  if (regex.test(r.title)) { // if there are any results
-				return regex.exec(r.title)[0]; // return them to searchResults
-			  } 
-		  
-			  console.log(regex);
-			});
-		  
-		   // filter blank entries from searchResults
-			searchResults = searchResults.filter(function(r){ 
-		  
-			  return r != undefined;
-			});
-			
-			// filter dataset with searchResults
-			searched_data = searchResults.map(function(r) {
-			   return data.filter(function(p) {
-			   return p.title.indexOf(r) != -1;
-			  })
-			});
-		  
-			// flatten array 
-			searched_data = [].concat.apply([], searched_data);
-			
-			// data bind with new data
-			rows = table.select("tbody").selectAll("tr")
-			.data(searched_data, function(d){ return d.id; });
-		  
-			// enter the rows
-			rows.enter()
-			 .append("tr");
-			 
-			// enter td's in each row
-			row_entries = rows.selectAll("td")
-				.data(function(d) { 
-				   arr = [];
-				  for ( k in d) {
-					if (d.hasOwnProperty(k)) {
-				  arr.push(d[k]);
-					}
-				  }
-				  return [arr[3],arr[1],arr[2],arr[0]];
-				})
-			  .enter()
-				.append("td");
-		  
-			// draw row entries with no anchor 
-			row_entries_no_anchor = row_entries.filter(function(d) {
-			  return (/https?:\/\//.test(d) == false)
-			});
-			row_entries_no_anchor.text(function(d) { return d; });
-		  
-			// draw row entries with anchor
-			row_entries_with_anchor = row_entries.filter(function(d) {
-			  return (/https?:\/\//.test(d) == true)  
-			});
-			row_entries_with_anchor
-			  .append("a")
-			  .attr("href", function(d) { return d; })
-			  .attr("target", "_blank")
-			.text(function(d) { return d; });
-			
-			// exit
-			rows.exit().remove();
-		  });
+  // the columns you'd like to display
+  columns = [
+    "School Name",
+    "Offers CS?",
+    "num_students",
+    "School District Name",
+    "County Name",
+  ];
+
+  (table = d3.select("#table-container").append("table")),
+    (thead = table.append("thead")),
+    (tbody = table.append("tbody"));
+
+  // append the header row
+  thead
+    .append("tr")
+    .selectAll("th")
+    .data(columns)
+    .enter()
+    .append("th")
+    .text(function (column) {
+      return column;
+    });
+
+  // create a row for each object in the data
+  rows = tbody.selectAll("tr").data(console.log(data)).enter().append("tr");
+
+  // create a cell in each row for each column
+  cells = rows
+    .selectAll("td")
+    .data(function (row) {
+      return columns.map(function (column) {
+        return { column: column, value: row[column] };
+      });
+    })
+    .enter()
+    .append("td")
+    .text(function (d) {
+      return d.value;
+    });
+
+  d3.select("#gene_search_box").on("keyup", function () {
+    // filter according to key pressed
+    (searched_data = data), (text = this.value.trim());
+
+    searchResults = searched_data.map(function (r) {
+      regex = new RegExp(text);
+      if (regex.test(r.title)) {
+        // if there are any results
+        return regex.exec(r.title)[0]; // return them to searchResults
+      }
+
+    });
+
+    // filter blank entries from searchResults
+    searchResults = searchResults.filter(function (r) {
+      return r != undefined;
+    });
+
+    // filter dataset with searchResults
+    searched_data = searchResults.map(function (r) {
+      return data.filter(function (p) {
+        return p.title.indexOf(r) != -1;
+      });
+    });
+
+    // flatten array
+    searched_data = [].concat.apply([], searched_data);
+
+    // data bind with new data
+    rows = table
+      .select("tbody")
+      .selectAll("tr")
+      .data(searched_data, function (d) {
+        return d.id;
+      });
+
+    // enter the rows
+    rows.enter().append("tr");
+
+    // enter td's in each row
+    row_entries = rows
+      .selectAll("td")
+      .data(function (d) {
+        arr = [];
+        for (k in d) {
+          if (d.hasOwnProperty(k)) {
+            arr.push(d[k]);
+          }
+        }
+        return [arr[3], arr[1], arr[2], arr[0]];
+      })
+      .enter()
+      .append("td");
+
+    // draw row entries with no anchor
+    row_entries_no_anchor = row_entries.filter(function (d) {
+      return /https?:\/\//.test(d) == false;
+    });
+    row_entries_no_anchor.text(function (d) {
+      return d;
+    });
+
+    // draw row entries with anchor
+    row_entries_with_anchor = row_entries.filter(function (d) {
+      return /https?:\/\//.test(d) == true;
+    });
+    row_entries_with_anchor
+      .append("a")
+      .attr("href", function (d) {
+        return d;
+      })
+      .attr("target", "_blank")
+      .text(function (d) {
+        return d;
+      });
+
+    // exit
+    rows.exit().remove();
   });
+});
